497839db25af5a2bfcb077bc9ab5d601ae4f46c0
Darth Vader <brave.pineapple@gmail.com>
2014-10-11 19:24:49 +0100

    .

--- PaaS/MainController.swift
+++ PaaS/MainController.swift
@@ -9,16 +9,46 @@
 import UIKit
 
 class MainController: UIViewController {
-
+    
+    var activeViewContainer: UIView? = nil;
+//    var activeController:    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.initController()
+        self.loadAuthView()
         
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+
         // Dispose of any resources that can be recreated.
+    }
+    
+    func initController() {
+        self.activeViewContainer = UIView(frame: UIScreen.mainScreen().bounds);
+        self.view.addSubview(self.activeViewContainer!);
+    }
+    
+    func loadAuthView () {
+        var authController = AuthController(nibName: "AuthController", bundle: nil);
+        self.setActiveViewWith(authController);
+    }
+    
+    func setActiveViewWith(controller:UIViewController) {
+        var activeSubviews = self.activeViewContainer?.subviews;
+        
+        if(activeSubviews?.count == 1) {
+            var view = activeSubviews![0] as UIView;
+            view.removeFromSuperview()
+        }
+        
+        self.activeViewContainer?.addSubview(controller.view);
+        
     }
     
 
