e45de72de365ed9c04b47894bd85392ef1a55b5e
Darth Vader <brave.pineapple@gmail.com>
2014-10-11 19:01:34 +0100

    main controller

--- PaaS/AppDelegate.swift
+++ PaaS/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        var mainController = MainController(nibName: "MainController", bundle: nil)
+        
+        self.window?.rootViewController = mainController
+        self.window?.backgroundColor    = UIColor.whiteColor()
+        
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
