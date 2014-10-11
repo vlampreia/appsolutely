a33396430b2b85bc2d90c882bf119e70c114da19
magicmark <mark@larah.me>
2014-10-11 18:33:48 +0100

    second commit lol

---
+++ PaaS/HTTP.swift
@@ -0,0 +1,33 @@
+//
+//  HTTP.swift
+//  PaaS
+//
+//  Created by Mark Larah on 11/10/2014.
+//  Copyright (c) 2014 Team Goat. All rights reserved.
+//
+
+import Foundation
+
+public class HTTP {
+    
+    var URL: NSURL? = nil;
+    
+    init (_URL: String) {
+        self.URL = NSURL(string: _URL);
+    }
+//    
+//    func Get (callback: (NSString)) -> Void? {
+//        
+//        if (self.URL == nil) {
+//            return nil;
+//        }
+//        
+//        let request = NSURLRequest(URL: self.URL!);
+//        
+//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+//            callback((NSString(data: data, encoding: NSUTF8StringEncoding)))
+//        }
+//        
+//    }
+    
+}
