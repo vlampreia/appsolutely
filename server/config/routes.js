e2304cfe67db1f786d4967bb449d4fa9be58e82c
vlampreia <victorlampreia@gmail.com>
2014-10-12 06:56:23 +0100

    CROSS SITE FUCKING

--- server/config/routes.js
+++ server/config/routes.js
@@ -42,6 +42,12 @@ var Routes = function() {
         users.isAuthenticated(req,res,next);
       });
 
+      app.all('/*', function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorisation-Token");
+        next();
+      });
+
       app.post('/login', injectDb, function(req, res, next) {
         users.login(req,res,next);
       });
@@ -54,6 +60,29 @@ var Routes = function() {
       app.post('/hello', function(req, res, next) {
       console.log('wtf');
         res.send('hello ' + req.param.qr);
+      });
+
+      app.get('/register', function(req, res, next) {
+        var html = '<!DOCTYPE html>'
+                  + '<html>'
+                    + '<head>'
+                      + '<script type="text/javascript" src="https://js.braintreegateway.com/v2/braintree.js"></script>'
+                    + '</head>'
+                    + '<body>'
+                      + '<form id="merchant-form" action="/create-transaction" method="post">'
+                        + '<input type="text" name="payment_method_nonce" id="payment-method-nonce" />'
+                        + '<div id="paypal-container"></div>'
+                        + '<input type="submit" value="Submit" />'
+                      + '</form>'
+                      + '<script type="text/javascript">'
+                        + 'braintree.setup("AY0mkBAZrqXgXkeDxVfEJXa1HL5-HPouajNkUK6thVVy39dlRb9BqBYBDKtW", "paypal", {'
+                        + 'container: "paypal-container",  // to specify DOM elements, use an ID, a DOM node, or a jQuery element'
+                        + 'paymentMethodNonceInputField: "payment-method-nonce"'
+                        + '});'
+                      + '</script>'
+                    + '</body>'
+                  + '</html>';
+        res.send(html);
       });
 
     }
